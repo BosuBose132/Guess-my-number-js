1f2513286f63775fbe2f7adf7abacd0d0d6980d7
Bosu <badebosubabu@gmail.com>
2025-10-26 19:27:22 -0400

    Implement the highscore feature to display the highest score

--- script.js
+++ script.js
@@ -13,19 +13,25 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = '?';
 
 let score = 20;
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
   // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No number!';
-  } // When player wins
-  else if (guess === secretNumber) {
+
+    // When player wins
+  } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // When guess is wrong
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -36,6 +42,7 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
     // When guess is lower
   } else if (guess < secretNumber) {
     if (score > 1) {
